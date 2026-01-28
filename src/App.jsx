fa8945ede0e6a698f3ec8754e7646009981a6a5e
jp1593 <juanpablo08082002@hotmail.com>
2026-01-28 10:55:03 -0600

    feat: add components to page structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { About } from "./components/sections/About";
+import { Experience } from "./components/sections/Experience";
+import { Hero } from "./components/sections/Hero";
+import { Projects } from "./components/sections/Projects";
+
 function App() {
   return (
     //Height of layout will be the total height of the screen size
-    <div className="min-h-screen over">
+    <div className="min-h-screen overflow-x-hidden">
       <NavBar>
+        <Hero />
+        <About />
+        <Projects />
+        <Experience />
       </NavBar>
     </div>
   )
