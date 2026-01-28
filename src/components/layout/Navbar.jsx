80896c2c992aa9cfa7512e9ad08c36ce9dddb674
jp1593 <juanpablo08082002@hotmail.com>
2026-01-28 12:46:44 -0600

    feat: add NavBar component

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,5 +1,39 @@
-export const NavBar = ()=> {
+// Button Imports
+import { Button } from "../Button"
+
+// Navigation Links
+const navLinks = [
+    { href: "#about", label: "About" },
+    { href: "#experience", label: "Experience" },
+    { href: "#hero", label: "Hero" },
+    { href: "#projects", label: "Projects" },
+
+]
+
+export const NavBar = () => {
     return (
-        <header></header>
+        <header className="fixed top-0 left-0 bg-transparent py-5 right-0 z-50">
+            <nav className="container mx-auto px-6 flex items-center justify-between">
+                <a href="#" className="text-xl font-bold tracking-tight hover:text-primary">
+                    JP<span className="text-primary">.</span>
+                </a>
+
+                {/* Desktop Nav */}
+                <div className="flex items-center gap-1">
+                    <div className="glass rounded-full px-2 py-1 items-center gap-1">
+                        {navLinks.map((link, index) => (
+                            <a href={link.href} key={index} className="px-4 py-2 text-muted-foreground hover:text-foreground rounded-full ">{link.label}</a>
+                        ))}
+                    </div>
+                </div>
+                {/* Call to action - BTN */}
+                <div>
+                    <Button size="sm">
+                        Contact Me
+                    </Button>
+                </div>
+            </nav>
+        </header>
+
     )
 }
