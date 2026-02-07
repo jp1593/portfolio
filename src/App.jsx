c9e237080ff1532fb81ff9b7349cdab5153b2eb5
jp1593 <juanpablo08082002@hotmail.com>
2026-02-07 16:07:17 -0600

    feat: add cert section template and add it into the routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { About } from "./components/sections/About";
 import { Experience } from "./components/sections/Experience";
 import { Hero } from "./components/sections/Hero";
 import { Projects } from "./components/sections/Projects";
+import { Certs } from "./components/sections/Certs";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <main>
         <Hero />
         <About />
+        <Certs />
         <Projects />
         <Experience />
       </main>
---
+++ src/components/sections/Certs.jsx
@@ -0,0 +1,5 @@
+export const Certs = ()=> {
+    return (
+        <section></section>
+    )
+}
