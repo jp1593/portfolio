cfedbcfe99b1c4e113319eb306b8d54c29e7bc34
jp1593 <juanpablo08082002@hotmail.com>
2026-02-07 15:14:21 -0600

    fix: add certifications option in navBar and readjust spacing

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react"
 // Navigation Links
 const navLinks = [
     { href: "#about", label: "About" },
+    { href: "#certifications", label: "Certifications" },
     { href: "#projects", label: "Projects" },
     { href: "#experience", label: "Experience" },
 ]
@@ -24,7 +25,7 @@ export const NavBar = () => {
 
     return (
         <header className={`fixed top-0 left-0 right-0 ${isScrolled ? "glass-strong py-3" : " bg-transparent py-5"} z-50`}>
-            <nav className="container ml-32 px-6 flex items-center justify-between">
+            <nav className="container ml-40 px-6 flex items-center justify-between">
                 <a href="#" className="relative flex items-center h-8 w-30">
                     <img
                         src={jpLogo}
@@ -36,7 +37,7 @@ export const NavBar = () => {
 
 
                 {/* Desktop Nav */}
-                <div className="flex items-center gap-1">
+                <div className="flex items-center gap-1 mr-8">
                     <div className="glass rounded-full px-2 py-1 items-center gap-1">
                         {navLinks.map((link, index) => (
                             <a href={link.href} key={index} className="px-4 py-2 text-muted-foreground hover:text-foreground rounded-full ">{link.label}</a>
